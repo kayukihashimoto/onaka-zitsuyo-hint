a435c278df9e3802e4f32a806d43952c7eb05a7a
kayukihashimoto <kfunnytokyo@gmail.com>
2024-07-19 21:17:17 +0900

    add compatibilityDate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
   app: {
     baseURL: '/onaka-zitsuyo-hint'
   },
-  devtools: { enabled: true },
   css: ['~/assets/scss/main.scss'],
   vite: {
     css: {
