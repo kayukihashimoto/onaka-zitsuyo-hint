5155552055fa5a0eafda2b5d00cedbbbd9cc3371
kayukihashimoto <kfunnytokyo@gmail.com>
2024-07-19 21:01:40 +0900

    add layouts

---
+++ components/organisms/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1>
+      <NuxtLink to="/">
+        おなかの不調と向き合う<br>
+        わたしの実用ヒント
+      </NuxtLink>
+    </h1>
+  </header>
+</template>
+
+<style lang="scss">
+header {
+  display: flex;
+  width: 100%;
+  height: 80px;
+  background: $secondary;
+  padding: 16px 16px 16px 24px;
+  justify-content: space-between;
+  align-items: center;
+
+  h1 {
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 1.6;
+  }
+}
+</style>
---
+++ pages/onaka-fucyo/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>
+      おなかの不調？
+    </h1>
+  </div>
+</template>
