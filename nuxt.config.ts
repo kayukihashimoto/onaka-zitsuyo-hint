7097fe1248e2c12247f50e9a7fcefb47ae8e4a67
kayukihashimoto <kfunnytokyo@gmail.com>
2024-07-22 23:19:44 +0900

    fix baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
 
-  app: {
-    baseURL: '/onaka-zitsuyo-hint'
-  },
-
-  css: ['~/assets/scss/main.scss'],
+  css: ["~/assets/scss/main.scss"],
 
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "~/assets/scss/_variables.scss" as *;'
-        }
-      }
-    }
+          additionalData: '@use "~/assets/scss/_variables.scss" as *;',
+        },
+      },
+    },
   },
 
-  modules: ["@nuxt/eslint"]
-})
+  modules: ["@nuxt/eslint"],
+});
