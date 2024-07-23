65669eeaaaa7afff31131407dd761a53d18c08b4
kayukihashimoto <kfunnytokyo@gmail.com>
2024-07-23 23:18:20 +0900

    fix styles

--- components/organisms/TheHeader.vue
+++ components/organisms/TheHeader.vue
@@ -92,11 +92,11 @@ header {
     &.is-open {
       display: block;
       content: '';
-      position: absolute;
+      position: fixed;
       top: 0;
       left: 0;
       width: 100%;
-      height: 100lvh;
+      height: 100svh;
       background: $bark-blue54;
     }
   }
