f1626858bbc0c62e43c831e36e23091bd40b0db5
kayukihashimoto <kfunnytokyo@gmail.com>
2024-07-24 20:20:13 +0900

    add googleFonts

--- app.vue
+++ app.vue
@@ -57,7 +57,3 @@ useHead({
     <NuxtPage />
   </NuxtLayout>
 </template>
-
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@400;500;700;900&display=swap');
-</style>
